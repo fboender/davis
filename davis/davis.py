41126faee1cceb0824dc2f25bb6669e78e3f1515
Ferry Boender <ferry.boender@electricmonk.nl>
2017-03-08 10:10:05 +0100

    Added value pane

--- davis/davis.py
+++ davis/davis.py
@@ -16,28 +16,63 @@ class MainInterface(object):
         self.window.title("Python Data Visualizer")
         self.window.bind("<Escape>", lambda e: e.widget.quit())
         self.window.geometry('700x500')
+        self.window.grid_rowconfigure(0, weight=1)
+        self.window.grid_columnconfigure(0, weight=1)
 
-        scrollbar = Scrollbar(self.window)
-        scrollbar.pack(side=RIGHT, fill=Y)
+        self.pane = PanedWindow(orient=VERTICAL)
+        self.pane.grid(row=0, column=0, sticky="nesw", in_=self.window)
+        self.pane.grid_rowconfigure(0, weight=1)
+        self.pane.grid_columnconfigure(0, weight=1)
 
+        tree_frame = self.mk_tree(self.pane)
+        self.tree_fill(self.tree, obj)
+        self.pane.add(tree_frame)
+
+        value_viewer_frame = self.mk_value_viewer(self.pane)
+        self.pane.add(value_viewer_frame)
+
+    def mk_tree(self, master):
         tree_columns = (
             ("type", "Type"),
             ("value", "Value"),
         )
-        self.tree = ttk.Treeview(self.window,
-                                 columns=[c[0] for c in tree_columns],
-                                 yscrollcommand=scrollbar.set)
+
+        frame = Frame(master, bg="#FF0000", bd=0)
+        frame.grid_rowconfigure(0, weight=1)
+        frame.grid_columnconfigure(0, weight=1)
+
+        tree = ttk.Treeview(frame,
+                            columns=[c[0] for c in tree_columns],
+                            selectmode=BROWSE)
         for c in tree_columns:
-            self.tree.heading(c[0], text=c[1])
-        self.tree.pack(side=TOP, expand=YES, fill=BOTH)
-        self.tree.bind('<Double-1>', self.ev_treeitem_doubleclicked)
+            tree.heading(c[0], text=c[1])
+        tree.bind('<<TreeviewSelect>>', self.ev_treeitem_selected)
+        tree.grid(row=0, column=0, sticky="nsew", in_=frame)
 
-        self.tree_fill(obj)
+        scrollbar = Scrollbar(frame, command=tree.yview)
+        scrollbar.grid(row=0, column=1, sticky="ns", in_=frame)
+        tree['yscrollcommand'] = scrollbar.set
 
-    def run(self):
-        self.window.mainloop()
+        self.tree = tree
+        return frame
 
-    def tree_fill(self, obj, parent=''):
+    def mk_value_viewer(self, master):
+        frame = Frame(master, bg="#0000FF", bd=0)
+        frame.grid_rowconfigure(0, weight=1)
+        frame.grid_columnconfigure(0, weight=1)
+
+        text = Text(frame)
+        text.insert('1.0', "hoi")
+        text.grid(row=0, column=0, sticky="nsew", in_=frame)
+
+        scrollbar = Scrollbar(frame, command=text.yview)
+        scrollbar.grid(row=0, column=1, sticky="ns", in_=frame)
+        text['yscrollcommand'] = scrollbar.set
+
+        self.text = text
+        return frame
+
+    def tree_fill(self, tree, obj, parent=''):
         obj_type = type(obj).__name__
 
         if obj_type == 'dict':
@@ -45,46 +80,33 @@ class MainInterface(object):
                 val_type = type(obj[val_name]).__name__
                 val_value = obj[val_name]
                 if val_type in recurse_types:
-                    item_id = self.tree.insert(parent, 'end', text=val_name, values=(val_type, ))
+                    item_id = tree.insert(parent, 'end', text=val_name, values=(val_type, ))
                     self.item_values[item_id] = val_value
-                    self.tree_fill(val_value, item_id)
+                    self.tree_fill(tree, val_value, item_id)
                 else:
-                    item_id = self.tree.insert(parent, 'end', text=val_name, values=(val_type, repr(val_value)))
+                    item_id = tree.insert(parent, 'end', text=val_name, values=(val_type, repr(val_value)))
                     self.item_values[item_id] = val_value
         elif obj_type in ('list', 'set', 'tuple'):
             i = 0
             for val_value in sorted(obj):
                 val_type = type(val_value).__name__
                 if val_type in recurse_types:
-                    item_id = self.tree.insert(parent, 'end', text=i, values=(val_type))
-                    self.tree_fill(val_value, item_id)
+                    item_id = tree.insert(parent, 'end', text=i, values=(val_type))
+                    self.tree_fill(tree, val_value, item_id)
                     self.item_values[item_id] = val_value
                 else:
-                    item_id = self.tree.insert(parent, 'end', text=i, values=(val_type, repr(val_value)))
+                    item_id = tree.insert(parent, 'end', text=i, values=(val_type, repr(val_value)))
                     self.item_values[item_id] = val_value
                 i += 1
         else:
-            item_id = self.tree.insert(parent, 'end', text=obj, values=(obj_type, ))
+            item_id = tree.insert(parent, 'end', text=obj, values=(obj_type, ))
             self.item_values[item_id] = obj
 
-    def ev_treeitem_doubleclicked(self, ev):
+    def ev_treeitem_selected(self, ev):
         item_id = self.tree.selection()[0]
         val_value = self.item_values[item_id]
-        v = ViewValueInterface(val_value)
-
-
-class ViewValueInterface:
-    def __init__(self, value):
-        self.window = Toplevel()
-        self.window.title("Value")
-        self.window.bind("<Escape>", lambda e: self.window.destroy())
-
-        scrollbar = Scrollbar(self.window)
-        scrollbar.pack(side=RIGHT, fill=Y)
-
-        self.text = Text(self.window, width=80, height=25, yscrollcommand=scrollbar.set)
-        self.text.pack(side=TOP, expand=YES, fill=BOTH)
-        self.text.insert('1.0', pprint.pformat(value))
+        self.text.delete(1.0, END)
+        self.text.insert("1.0", pprint.pformat(val_value))
 
     def run(self):
         self.window.mainloop()
